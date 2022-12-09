bbeb2588d066b1a0d620a0f217ecd3dfc7216fc3
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2022-12-09 19:00:02 +0100

    Error handling 2 	modified:   events/errors.py

--- events/errors.py
+++ events/errors.py
@@ -2,7 +2,7 @@ import logging
 import traceback
 from io import BytesIO, StringIO
 from typing import Any
-
+import datetime
 from discord import Embed, File, Interaction
 from discord.app_commands.errors import AppCommandError, CommandNotFound
 from discord.ext import commands
@@ -25,7 +25,9 @@ class Errors(commands.Cog):
             return interaction.response.send_message
 
     async def app_command_error(self, interaction: Interaction, error: AppCommandError): # TODO Do more tests for discord.py exceptions.
-        embed = None
+        embed: Embed | None = None
+        rightNow = datetime.datetime.now()
+        rightNow = rightNow.strftime('Date: **%d/%m/%Y**\nTime: **%H:%M:%S**')
 
         # exception_list = traceback.format_exception(type(error), error, error.__traceback__)
         # exception_string = "".join(exception_list).encode("utf-8")
@@ -39,8 +41,9 @@ class Errors(commands.Cog):
         elif not isinstance(error, CommandNotFound):
             buffer = StringIO()
             embed = Embed(
-                title="It seems like I've ran into a problem.",
-                color=0xff0000
+                title="Unhandled exception",
+                color=0xff0000,
+                description=f"Command: **/{interaction.command.name}**\nUsed by: **[{interaction.user}](https://discord.com/users/{interaction.user.id})**\n{rightNow}"
             )
 
             traceback.print_exception(
@@ -50,9 +53,19 @@ class Errors(commands.Cog):
             buffer.seek(0)
             buffer = BytesIO(buffer.getvalue().encode("utf-8"))
 
-            await self.send(interaction)(
+            owner_appdetails = await interaction.client.application_info()
+            await owner_appdetails.owner.send(
                 embed=embed,
-                file=File(buffer, "traceback.txt"),
+                file=File(buffer, "traceback.txt")
+            )
+
+            embed = Embed(
+                title="It seems like I've ran into a problem. I've already reported the issue to the developer.",
+                color=0xff0000
+            )
+
+            await self.send(interaction)(
+                embed=embed
             )
 
             DSLOGGER.error('Unhandled exception:', exc_info=(type(error), error, error.__traceback__))
