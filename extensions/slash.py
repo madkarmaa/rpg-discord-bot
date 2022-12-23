d19f81f7c87e884c3d7488547f97ca7d428279e4
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2022-12-23 19:38:26 +0100

    Misc 	modified:   extensions/slash.py

--- extensions/slash.py
+++ extensions/slash.py
@@ -26,12 +26,14 @@ class Slash(commands.Cog):
         # test = await self.bot._data_database_manager.get_weapons_specials("invalid", "test1")
         await interaction.response.send_message(test)
 
-    @app_commands.command()
-    async def test2(self, interaction: Interaction):
+    @app_commands.command()  # ? See https://discordpy.readthedocs.io/en/stable/interactions/api.html#decorators
+    @app_commands.rename(weapon_name="weapon")
+    @app_commands.describe(weapon_name="The base weapon to search its special variants from.")
+    async def test2(self, interaction: Interaction, weapon_name: str):
         """Test command 2."""
         starting_page: int = 0
         embeds: list[discord.Embed] = []
-        for weapon in await self.bot._data_database_manager.get_weapons_specials("melee", "axe"):
+        for weapon in await self.bot._data_database_manager.get_weapons_specials("melee", weapon_name):
 
             embed: discord.Embed = discord.Embed(title=weapon.get("name"), description=weapon.get("description"))
 
